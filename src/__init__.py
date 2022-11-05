1b2919ed6088ee3670839e94d4f87c94f3bf8c43
ReynardZ <78435005+ReynardZ@users.noreply.github.com>
2022-11-05 18:29:57 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,7 @@ from .apps.tickets_opp import to
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
-app.SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL?sslmode=require').replace('postgres://', 'postgresql://')
+app.SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 app.register_blueprint(main)
 app.register_blueprint(auth)
 app.register_blueprint(to)
