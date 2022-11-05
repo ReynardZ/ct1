d77eb370c9f8fd4241816a8eb0875affad8bda5e
ReynardZ <78435005+ReynardZ@users.noreply.github.com>
2022-11-05 19:18:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from src import app, db_init
 from src.models import Models
 
-def create_app()
+def create_app():
     models = Models()
     models.initialize()
     db_init.init_database('src/data.sql', models)
