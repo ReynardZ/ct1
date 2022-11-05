0d53d7fa27671234eb553b5c078e61ae222851e8
ReynardZ <78435005+ReynardZ@users.noreply.github.com>
2022-11-05 17:05:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from ct1.src import app
 from src.db_init import init_database
 from src.models import Models
 
-if __name__ == '__main__':
+
+def create_app():
     models = Models()
     models.initialize()
     init_database('src/data.sql', models)
-    app.run()
+    return app
+
+
+application = create_app()
