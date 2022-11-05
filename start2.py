75fa3d95ca7ab40c57367a465d3ecf22e9cb5b99
ReynardZ <78435005+ReynardZ@users.noreply.github.com>
2022-11-05 20:52:38 +0800

    Update start2.py

--- start2.py
+++ start2.py
@@ -3,6 +3,6 @@ from src.models import Models
 
 if __name__ == '__main--':
     models = Models()
-    models.initialize()
-    db_init.init_database('src/data.sql', models)
+    #models.initialize()
+    #db_init.init_database('src/data.sql', models)
     app.run()
