4b05168f7c6d61205de988781994026a26e1d743
ReynardZ <78435005+ReynardZ@users.noreply.github.com>
2022-11-05 18:23:12 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -d ".venv" ]
+then
+    source .venv/bin/activate
+    python3 app.py
+else
+    python3 -m venv .venv
+    source .venv/bin/activate
+    python3 -m pip install --upgrade pip
+    pip install -r requirements.txt
+    python3 app.py
+fi
